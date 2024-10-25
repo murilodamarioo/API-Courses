3dc2009f1ed7c00d4434a052e6c7dc0fddd3c097
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-24 21:41:14 -0300

    First commit

---
+++ src/main/java/com/courses/zonelearn/ZonelearnApplication.java
@@ -0,0 +1,13 @@
+package com.courses.zonelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonelearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonelearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courses/zonelearn/ZonelearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.zonelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonelearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
