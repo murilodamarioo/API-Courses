bc4ef0a54cd29b24fa269fe54cebfc681928913b
murilodamarioo <murilo.gcdamario@gmail.com>
2024-10-28 20:13:54 -0300

    refactor: add Id field

--- src/main/java/com/courses/zonelearn/modules/course/dto/CourseDTO.java
+++ src/main/java/com/courses/zonelearn/modules/course/dto/CourseDTO.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Data
 public class CourseDTO {
+    private UUID id;
     private String name;
     private String category;
     private Status status;
