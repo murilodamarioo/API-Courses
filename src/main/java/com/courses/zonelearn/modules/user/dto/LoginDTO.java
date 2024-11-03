a7a215723f7905161a20290e15eb0bacee006170
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-03 18:42:52 -0300

    refactor: add annotations and fields for RegisterDTO

--- src/main/java/com/courses/zonelearn/modules/user/dto/LoginDTO.java
+++ src/main/java/com/courses/zonelearn/modules/user/dto/LoginDTO.java
@@ -2,9 +2,11 @@ package com.courses.zonelearn.modules.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
 
     private String email;
--- src/main/java/com/courses/zonelearn/modules/user/dto/RegisterDTO.java
+++ src/main/java/com/courses/zonelearn/modules/user/dto/RegisterDTO.java
@@ -3,11 +3,15 @@ package com.courses.zonelearn.modules.user.dto;
 import com.courses.zonelearn.modules.user.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterDTO {
 
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     private Role role;
