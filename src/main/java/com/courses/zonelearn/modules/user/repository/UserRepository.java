7b066103296bfa02cbbe5e052da106a10427ae27
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-03 18:44:20 -0300

    feat: implement findByEmail method

--- src/main/java/com/courses/zonelearn/modules/user/repository/UserRepository.java
+++ src/main/java/com/courses/zonelearn/modules/user/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.courses.zonelearn.modules.user.repository;
 import com.courses.zonelearn.modules.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
