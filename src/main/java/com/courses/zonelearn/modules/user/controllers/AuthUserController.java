55c4f36af7435b38a8dd8cd8c680d1f829bbb544
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-03 22:56:18 -0300

    feat: implement auth usecase and controller

---
+++ src/main/java/com/courses/zonelearn/modules/user/controllers/AuthUserController.java
@@ -0,0 +1,27 @@
+package com.courses.zonelearn.modules.user.controllers;
+
+import com.courses.zonelearn.modules.user.dto.LoginDTO;
+import com.courses.zonelearn.modules.user.usecases.AuthUserUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.AuthenticationException;
+
+@RestController
+@RequestMapping("user")
+public class AuthUserController {
+
+    @Autowired
+    AuthUserUseCase authUserUseCase;
+
+    @PostMapping("/auth")
+    public ResponseEntity<Object> create(@RequestBody LoginDTO loginDTO) {
+        var response = this.authUserUseCase.execute(loginDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
---
+++ src/main/java/com/courses/zonelearn/modules/user/usecases/AuthUserUseCase.java
@@ -0,0 +1,50 @@
+package com.courses.zonelearn.modules.user.usecases;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.courses.zonelearn.exceptions.EmailOrPasswordInvalidException;
+import com.courses.zonelearn.modules.user.dto.LoginDTO;
+import com.courses.zonelearn.modules.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
+
+@Service
+public class AuthUserUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String execute(LoginDTO loginDTO) {
+        var user = this.repository.findByEmail(loginDTO.getEmail()).orElseThrow(
+                () -> new EmailOrPasswordInvalidException("E-mail/Password incorrect")
+        );
+
+        var passwordMatches = this.passwordEncoder.matches(loginDTO.getPassword(), user.getPassword());
+
+        if (!passwordMatches) {
+            throw new EmailOrPasswordInvalidException("E-mail/Password incorrect");
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        // Generate token
+
+        return JWT.create()
+                .withIssuer("@zonelearn")
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+                .withSubject(user.getId())
+                .sign(algorithm);
+    }
+}
