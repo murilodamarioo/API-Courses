a4ec5f28a0cfc14fa038622dca95d12b0047e519
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-03 22:03:46 -0300

    feat: implement handleValidationExceptions

--- src/main/java/com/courses/zonelearn/infra/GlobalExceptionHandler.java
+++ src/main/java/com/courses/zonelearn/infra/GlobalExceptionHandler.java
@@ -2,10 +2,10 @@ package com.courses.zonelearn.infra;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,5 +14,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleNotFound(NoHandlerFoundException exception) {
         var threatResponse = new RestErrorMessage(HttpStatus.NOT_FOUND, "The route was not found. Check the URL");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RestErrorMessage> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        StringBuilder errorMessages = new StringBuilder("Validation error(s)- ");
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMessages.append(String.format("%s: %s; ", error.getField(), error.getDefaultMessage()));
+        });
+
+        var threatResponse = new RestErrorMessage(HttpStatus.BAD_REQUEST, errorMessages.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(threatResponse);
     }
 }
