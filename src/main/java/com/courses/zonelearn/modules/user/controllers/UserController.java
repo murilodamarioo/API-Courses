fd6265f07f81a049bb6024bcdc64230f965e9db4
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-09 22:19:04 -0300

    feat: implement profile route

--- src/main/java/com/courses/zonelearn/modules/user/controllers/UserController.java
+++ src/main/java/com/courses/zonelearn/modules/user/controllers/UserController.java
@@ -2,14 +2,12 @@ package com.courses.zonelearn.modules.user.controllers;
 
 import com.courses.zonelearn.modules.user.entities.User;
 import com.courses.zonelearn.modules.user.usecases.CreateUserUseCase;
+import com.courses.zonelearn.modules.user.usecases.GetProfileUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
@@ -18,9 +16,18 @@ public class UserController {
     @Autowired
     private CreateUserUseCase createUserUseCase;
 
+    @Autowired
+    private GetProfileUseCase getProfileUseCase;
+
     @PostMapping
     public ResponseEntity<Object> create(@Valid @RequestBody User user) {
         var response = this.createUserUseCase.execute(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<Object> get(@RequestHeader("Authorization") String sub) {
+        var response = this.getProfileUseCase.execute(sub);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
