1bc3ffdf2e92491f759ae44a13d4d2cf1974eab5
murilodamarioo <murilo.gcdamario@gmail.com>
2024-11-09 22:18:38 -0300

    feat: create GetProfileUseCase

---
+++ src/main/java/com/courses/zonelearn/modules/user/usecases/GetProfileUseCase.java
@@ -0,0 +1,39 @@
+package com.courses.zonelearn.modules.user.usecases;
+
+import com.courses.zonelearn.modules.user.dto.ProfileUserResponseDTO;
+import com.courses.zonelearn.modules.user.entities.User;
+import com.courses.zonelearn.modules.user.enums.Role;
+import com.courses.zonelearn.modules.user.repository.UserRepository;
+import com.courses.zonelearn.providers.JWTProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class GetProfileUseCase {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private JWTProvider jwtProvider;
+
+    public ProfileUserResponseDTO execute(String sub) {
+        String token = sub.replace("Bearer ", "").trim();
+        String userId = this.jwtProvider.getSubFromJwt(token);
+        UUID id = UUID.fromString(userId);
+
+        User user = this.repository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("Cannot show profile")
+        );
+
+        ProfileUserResponseDTO responseDTO = new ProfileUserResponseDTO();
+        responseDTO.setFullName(user.getFirstName() + " " + user.getLastName());
+        responseDTO.setEmail(user.getEmail());
+        responseDTO.setRole(user.getRole());
+
+        return responseDTO;
+    }
+}
